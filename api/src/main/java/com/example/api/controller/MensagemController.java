5ff3e19ca55e422a7ee75cb7f2a23ef98aa71880
JoaovlLima <jl949615@gmail.com>
2025-05-04 15:56:17 -0300

    04-05

--- api/src/main/java/com/example/api/controller/MensagemController.java
+++ api/src/main/java/com/example/api/controller/MensagemController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.service.ChatService;
+import com.example.api.service.GeraSqlService;
 import com.example.api.service.MensagemService;
 import com.example.api.service.RespostaService;
 
@@ -22,17 +23,21 @@ public class MensagemController {
     private final MensagemService mensagemService;
     private final RespostaService respostaService;
     private final ChatService chatService;
+    private final GeraSqlService geraSqlService;
 
-    public MensagemController(MensagemService mensagemService, RespostaService respostaService, ChatService chatService){
+    public MensagemController(MensagemService mensagemService, RespostaService respostaService, ChatService chatService, GeraSqlService geraSqlService){
         this.mensagemService = mensagemService;
         this.respostaService = respostaService;
         this.chatService = chatService;
+        this.geraSqlService = geraSqlService;
     }
    @PostMapping()
 
    public String buscarIntencao(@RequestBody String mensagem) {
        String intencao = mensagemService.buscarIntencao(mensagem);
-       String respostaSql = respostaService.buscarResposta(intencao);
+        
+       String sqlGerado = geraSqlService.geraSql(intencao, mensagem);
+       String respostaSql = respostaService.buscarResposta(sqlGerado);
        String respostaFinal = chatService.gerarResposta(mensagem, intencao, respostaSql);
 
        return respostaFinal;
